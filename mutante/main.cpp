0936b4fe60354e853741cd128c57d9251d2bac8a
Zetolac <58708564+Zetolac@users.noreply.github.com>
2021-11-07 17:05:08 +0100

    Add files via upload

---
+++ mutante/main.cpp
@@ -0,0 +1,119 @@
+/*
+ * Mutante
+ * Made by Samuel Tulach
+ * https://github.com/SamuelTulach/mutante
+ */
+
+#include <ntifs.h>
+#include "log.h"
+#include "shared.h"
+
+#include "disks.h"
+#include "smbios.h"
+#include <ntifs.h>
+#include <ntddk.h>
+#include <ntdddisk.h>
+#include <scsi.h>
+#include <intrin.h>
+
+extern "C" {
+	NTKERNELAPI NTSTATUS IoCreateDriver(PUNICODE_STRING DriverName, PDRIVER_INITIALIZE InitializationFunction);
+	
+	NTKERNELAPI PVOID PsGetProcessSectionBaseAddress(PEPROCESS Process);
+}
+/**
+ * \brief Driver's main entry point
+ * \param object Pointer to driver object (invalid when manual mapping)
+ * \param registry Registry path (invalid when manual mapping)
+ * \return Status of the driver execution
+ */
+NTSTATUS unsupported_io(PDEVICE_OBJECT device_obj, PIRP irp) {
+	irp->IoStatus.Status = STATUS_NOT_SUPPORTED;
+	IoCompleteRequest(irp, IO_NO_INCREMENT);
+	return irp->IoStatus.Status;
+}
+
+NTSTATUS create_io(PDEVICE_OBJECT device_obj, PIRP irp) {
+	UNREFERENCED_PARAMETER(device_obj);
+
+	IoCompleteRequest(irp, IO_NO_INCREMENT);
+	return irp->IoStatus.Status;
+}
+
+NTSTATUS close_io(PDEVICE_OBJECT device_obj, PIRP irp) {
+	UNREFERENCED_PARAMETER(device_obj);
+	IoCompleteRequest(irp, IO_NO_INCREMENT);
+	return irp->IoStatus.Status;
+}
+#define ctl_spoof    CTL_CODE(FILE_DEVICE_UNKNOWN, 0x0366, METHOD_BUFFERED, FILE_SPECIAL_ACCESS)
+NTSTATUS ioctl_spoofer(PDEVICE_OBJECT device_obj, PIRP irp) {
+	irp->IoStatus.Status = STATUS_SUCCESS;
+	irp->IoStatus.Information = sizeof(NULL);
+
+	auto stack = IoGetCurrentIrpStackLocation(irp);
+	//auto buffer = (p_info)irp->AssociatedIrp.SystemBuffer;
+
+	size_t size = 0;
+
+	if (stack) {
+		
+			if (stack->Parameters.DeviceIoControl.IoControlCode == ctl_spoof) {
+				Disks::DisableSmart();
+               Disks::ChangeDiskSerials();
+               Smbios::ChangeSmbiosSerials();
+			}
+
+		
+	}
+
+	IoCompleteRequest(irp, IO_NO_INCREMENT);
+
+	return irp->IoStatus.Status;
+}
+NTSTATUS HookedDriver(PDRIVER_OBJECT driver_obj, PUNICODE_STRING registery_path) {
+	auto  status = STATUS_SUCCESS;
+	UNICODE_STRING  sym_link, dev_name;
+	PDEVICE_OBJECT  dev_obj;
+
+	RtlInitUnicodeString(&dev_name, L"\\Device\\MyFirstIoctlPastedSpoofer");
+	status = IoCreateDevice(driver_obj, 0, &dev_name, FILE_DEVICE_UNKNOWN, FILE_DEVICE_SECURE_OPEN, FALSE, &dev_obj);
+
+	if (status != STATUS_SUCCESS) {
+		return status;
+	}
+
+	RtlInitUnicodeString(&sym_link, L"\\DosDevices\\MyFirstIoctlPastedSpoofer");
+	status = IoCreateSymbolicLink(&sym_link, &dev_name);
+
+	if (status != STATUS_SUCCESS) {
+		return status;
+	}
+
+	dev_obj->Flags |= DO_BUFFERED_IO;
+
+	for (int t = 0; t <= IRP_MJ_MAXIMUM_FUNCTION; t++)
+		driver_obj->MajorFunction[t] = unsupported_io;
+
+	driver_obj->MajorFunction[IRP_MJ_CREATE] = create_io;
+	driver_obj->MajorFunction[IRP_MJ_CLOSE] = close_io;
+	driver_obj->MajorFunction[IRP_MJ_DEVICE_CONTROL] = ioctl_spoofer;
+	driver_obj->DriverUnload = NULL;
+
+	dev_obj->Flags &= ~DO_DEVICE_INITIALIZING;
+
+	return status;
+}
+extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT object, PUNICODE_STRING registry)
+{
+	UNREFERENCED_PARAMETER(object);
+	UNREFERENCED_PARAMETER(registry);
+
+
+	auto        status = STATUS_SUCCESS;
+	UNICODE_STRING  drv_name;
+
+	RtlInitUnicodeString(&drv_name, L"\\Driver\\MyFirstIoctlPastedSpoofer");
+	status = IoCreateDriver(&drv_name, &HookedDriver);
+
+	return STATUS_SUCCESS;
+}
